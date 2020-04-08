f26d6ce94402800fd88e08cfb16e478fb1d18cff
Nicholas Heo <nicholas.heo95@gmail.com>
2020-04-08 13:44:24 -0400

    post request done for cassandra and postgres

--- cassandra/index.js
+++ cassandra/index.js
@@ -31,6 +31,27 @@ app.get("/api/airbnb.users/", (req, res) => {
   });
 });
 
+app.post("/api/airbnb.users/", (req, res) => {
+  var body = req.body.body;
+  var name = req.body.name;
+  var user_id = req.body.user_id;
+  var imageUrl = req.body.imageUrl;
+  var cleanliness = req.body.cleanliness;
+  var communication = req.body.communication;
+  var value = req.body.value;
+  var accuracy = req.body.accuracy;
+  var checkIn = req.body.checkIn;
+  var location = req.body.location;
+  var query = `insert into users(user_id,name,imageUrl, body, cleanliness,communication, value, accuracy, checkIn, location) values (${user_id},'${name}','${imageUrl}','${body}',${cleanliness},${communication},${value},${accuracy},${checkIn},${location})`;
+  client.execute(query, (err, results) => {
+    if (err) {
+      console.log(`error from post request(cassandra)`, err);
+    }
+    console.log("successfully sent to database");
+    res.end();
+  });
+});
+
 app.listen(port, () => {
   console.log(`app is running on port 3001`);
 });
